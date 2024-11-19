aba7cd33fd837341caae2c5eed241bf9f2a03bea
Kili S <79143155+klinshy@users.noreply.github.com>
2024-11-19 23:04:15 +0100

    initial push

---
+++ tilesets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="soccer" tilewidth="32" tileheight="32" tilecount="960" columns="30">
+ <image source="tileset.png" width="960" height="1024"/>
+</tileset>
